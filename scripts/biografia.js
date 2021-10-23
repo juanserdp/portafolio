b1d88e93726807a2384bf766fd1c8f50221d79a5
juanserdp <90122446+juanserdp@users.noreply.github.com>
2021-10-23 08:33:09 -0500

    biografia

---
+++ scripts/biografia.js
@@ -0,0 +1,19 @@
+function viaje(){
+    let v = document.createElement("A");
+    v.style.display="none";
+    v.href="#viaje";
+    v.click()
+}
+function contacto(){
+    let c = document.createElement("A");
+    c.style.display="none";
+    c.href="#contacto";
+    c.click()
+}
+function navegador(){
+    let b = document.createElement("A");
+    b.style.display="none";
+    b.href="#navegador";
+    b.click()
+}
+
